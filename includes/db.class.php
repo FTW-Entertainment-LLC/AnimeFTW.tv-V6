cc0ab2f6caaf65bd4801a875430dd049a3f71fdf
robotman321 <brad@ftwentertainment.com>
2016-05-21 22:20:39 -0500

    Made more updates to the database class to better support responses.

--- includes/db.class.php
+++ includes/db.class.php
@@ -7,16 +7,22 @@
 ## Copyright 2016 FTW Entertainment LLC, All Rights Reserved
 \****************************************************************/
 
-class DB extends Config
-{    
-    var $queryStatement;
+class DB
+{
     var $queryHash;
     var $queryCount;
     var $mysqli;
     var $queryResults;
+    var $dbhost, $dbuser, $dbpass, $dbname, $redisNodes;
 
-    public function __construct()
+    public function __construct($databaseInformation)
     {
+        // build out the database values.
+        $this->dbhost = $databaseInformation['host'];
+        $this->dbuser = $databaseInformation['user'];
+        $this->dbpass = $databaseInformation['pass'];
+        $this->dbname = $databaseInformation['db'];
+        $this->redisNodes = $databaseInformation['redisNodes'];
     }
     
     private function dbConnect()
@@ -43,42 +49,37 @@ class DB extends Config
         // each query will "expire" one hour after it was submitted. Now, we CAN override this functionality by setting the cache override.
         // we will want certain functions that are administrative in nature to be always on. Cached queries for regular members is fine.
         
-        $this->queryStatement = $query;
-        
         // Check for query type.
         if (substr($query, 0, 11) == "INSERT INTO") {
             // Insert statement
-            $this->insert();
+            $this->insert($query);
         } elseif (substr($query, 0, 6) == "UPDATE") {
             // Update statement
-            $this->update();
+            $this->update($query);
         } elseif (substr($query, 0, 6) == "SELECT") {
-            //Select statement        
-            // setting up the query hash
-            $this->hashQuery();
-            
+            //Select statement
             if($cacheOverride == false) {
                 // check to see if redis has the data.
-                $redisOutput = $this->selectRedis();
+                $redisOutput = $this->selectRedis($query);
                 // return the data.
                 $this->queryResults = $redisOutput['results'];
             } else {
                 // We are overriding the ability to get to the cache, so we will query mysql directly.
-                $this->queryResults = $this->selectQuery();
+                $this->queryResults = $this->selectQuery($query);
             }
         } else {
             die("Query type is unknown in database class.");
         }
     }
     
-    private function insert()
+    private function insert($query)
     {
         // Open up a connection to the database.
         $this->dbConnect();
         
         try {
             // try to submit the query.
-            $result = $this->mysqli->query($this->queryStatement);
+            $result = $this->mysqli->query($query);
             
         } catch (UpdateException $e) {
             // a failure happened.
@@ -89,13 +90,13 @@ class DB extends Config
         $this->dbDisconnect();
     }
     
-    private function update()
+    private function update($query)
     {
         // Open up a connection to the database.
         $this->dbConnect();
         
         try {
-            $result = $this->mysqli->query($this->queryStatement);
+            $result = $this->mysqli->query($query);
         } catch (UpdateException $e) {
             return "Error processing the update function, ${e}";
         }
@@ -109,36 +110,46 @@ class DB extends Config
         return $this->queryResults;
     }
     
-    private function selectQuery($cache = false)
+    private function selectQuery($query,$cache = false)
     {
         // Open up a connection to the database.
         $this->dbConnect();
         
         // Query the mysql database.
-        $result = $this->mysqli->query($this->queryStatement);
+        $result = $this->mysqli->query($query);
         
-        $rows = $result->fetch_assoc();
+        $data = array();
+        
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
         
         // clean up the connection
         $this->dbDisconnect();
         
-        return $rows;
+        return $data;
     }
     
-    private function selectRedis()
+    private function selectRedis($query)
     {
         // first thing, connect to redis
         $obj_cluster = new RedisCluster(NULL, $this->redisNodes);
         
+        // build the query hash
+        $hash = $this->hashQuery($query);
+        
         // check to see if the hash exists.
-        $redisData = $obj_cluster->get($this->queryHash);
+        $redisData = $obj_cluster->get($hash);
         
         if ($redisData == FALSE || (isset($redisData['expire']) && time() > $redisData['expire'])) {
+            echo '<!-- mysql hit, Query: ' . $query . ', Hash: ' . $hash . ' -->';
             // no data was available, or it was expired, so we need to readd/add the data to redis, then print it back out.
-            $data = $this->selectQuery();
+            $data = $this->selectQuery($query);
             
             // Add it to redis.
-            if ($this->insertIntoRedis($data) == FALSE) {
+            $redisInsert = $this->insertIntoRedis($data,$hash);
+            
+            if ($redisInsert == FALSE) {
                 // cycle through it again and attempt one more redo of the insert.
                 $value = $this->insertIntoRedis($data);
             }
@@ -147,13 +158,18 @@ class DB extends Config
             return array('results' => $data);
         } else {
             // There is redis data, give it back.
-            return array('results' => $redisData['results']);
+            // first, decode the json to an array
+            $data = json_decode($redisData, true);
+            echo '<!-- redis hit, Hash: ' . $hash . ' -->';
+            // return the data.
+            return array('results' => $data['results']);
         }
+        unset($hash);
     }
     
     // Insert data into redis.
     // We use the hashed value of the query to store the data in redis.
-    private function insertIntoRedis($results)
+    private function insertIntoRedis($results,$hash)
     {
         $current = time();
         $expire = time()+60;
@@ -161,13 +177,30 @@ class DB extends Config
         // Create a cluster setting two nodes as seeds
         $obj_cluster = new RedisCluster(NULL, $this->redisNodes);
         
-        $result = $obj_cluster->set($this->queryHash, array('timestamp' => $current, 'expire' => $expire, 'results' => $results));
+        $result = $obj_cluster->set($hash, json_encode(array('timestamp' => $current, 'expire' => $expire, 'results' => $results)));
         
         return $result;
     }
     
-    private function hashQuery()
+    private function hashQuery($query)
     {
-        $this->queryHash = md5($this>queryStatement);
+        return md5($query);
+    }
+    
+    public function escape($data)
+    {
+        // this is a manual way to escape strings.. while we want a connection to do this, we also want to keep the same hash across redis so that its not ever changing.
+        $replacements = array(
+            "\x00"=>'\x00',
+            "\n"=>'\n',
+            "\r"=>'\r',
+            "\\"=>'\\\\',
+            "'"=>"\'",
+            '"'=>'\"',
+            "\x1a"=>'\x1a'
+        );
+        
+        // escapes the input.
+        return strtr($data,$replacements);;
     }
 }
